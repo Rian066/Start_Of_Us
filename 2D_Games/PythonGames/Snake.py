ad990f8d3a6090c33b56ae4dd5009eb54913582f
MacBook Pro <rianalif16@gmail.com>
2024-09-07 01:43:20 -0400

    Setup the screen for game

--- 2D_Games/PythonGames/Snake.py
+++ 2D_Games/PythonGames/Snake.py
@@ -1,5 +1,14 @@
 #Starting snake game
 
+import tkinter
 import turtle
 
 snake = turtle.Turtle()
+
+window = turtle.Screen()
+
+window.bgcolor("cyan")
+snake.color("black")
+
+
+window.exitonclick()
