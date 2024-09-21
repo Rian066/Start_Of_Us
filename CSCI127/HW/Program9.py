281f4eaab2fa16fe712b814b86c684dbf6822ab9
MacBook Pro <rianalif16@gmail.com>
2024-09-21 02:25:47 -0400

    Finished program9 for CSCI127 HW

---
+++ CSCI127/HW/Program9.py
@@ -0,0 +1,16 @@
+#Name:  Rian M Alif
+#Email: rian.alif58@myhunter.cuny.edu
+#Date: September 21, 2024
+#This program gives the turtle a color based on the input
+
+import turtle
+
+color = input("Enter a hex string: ")
+hex_turtle = turtle.Turtle()
+
+# Characteristics 
+hex_turtle.shape("turtle")  # Shape is like a turtle
+hex_turtle.shapesize(5)  # Size is 5 times the default
+hex_turtle.color(color)
+
+turtle.exitonclick()
